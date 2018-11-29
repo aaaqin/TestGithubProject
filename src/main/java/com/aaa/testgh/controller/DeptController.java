40303af33c66c2fa445a496356f4b77994320a44
jnqs <19663966362@qq.com>
2018-11-29 09:56:49 +0800

    秦帅

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:qin
+ * createTime:2018-11-29 09:08
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("hello!!!!!!");
+    }
+}
---
+++ src/main/java/com/aaa/testgh/controller/EmpController.java
@@ -0,0 +1,13 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:EmpController
+ * discription:
+ * author:qin
+ * createTime:2018-11-29 09:43
+ */
+public class EmpController {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
